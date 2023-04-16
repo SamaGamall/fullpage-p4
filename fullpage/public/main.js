8cfd6a34ae10c26058b350a8268652a365d0185d
SamaGamall <samagamal024@gmail.com>
2023-04-16 04:45:46 +0200

    first commit

---
+++ fullpage/public/main.js
@@ -0,0 +1,13 @@
+
+window.addEventListener("scroll",function(){
+   var header =this.document.querySelector("header");
+   header.classList.toggle("sticky",this.window.scrollY>0);
+})
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "nav-bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-bar";
+  }
+}
